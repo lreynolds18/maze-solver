5f8025d73c2046ad6dabd37e51c1e8c2d8f72b73
Lucas Reynolds <lreynolds18@gmail.com>
2017-11-10 20:26:50 -0500

    Comments to satisify rust docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![deny(missing_docs)]
+
 extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
@@ -10,6 +12,12 @@ use self::glutin_window::GlutinWindow as Window;
 use self::opengl_graphics::{ GlGraphics, OpenGL };
 use self::graphics::*;
 
+
+/// Creates a maze using a popular algorithm
+/// First element in return tuple is 2d vector
+/// Second element in return tuple is array with first element 
+/// corresponds to length of maze in x dimension.
+/// Second element is length of maze in y dimension.
 fn generate_maze() -> (Vec<Vec<u32>>, [usize; 2]) {
     (vec![vec![0, 0, 1, 1, 1, 1],
           vec![1, 0, 0, 0, 0, 1],
@@ -20,24 +28,33 @@ fn generate_maze() -> (Vec<Vec<u32>>, [usize; 2]) {
     ], [6, 6])
 }
 
+/// Hold datas of path already explored
 struct MazeSolver {
 }
 
 impl MazeSolver {
+    /// Implementation of Depth First Search
     fn dfs() {}
 
+    /// Implementation of Breadth First Search
     fn bfs() {}
 
+    /// Implementation of A* Search
     fn a_star() {}
 }
 
+/// Holds objects needed to create a graphics window
 struct Screen {
-    gl: GlGraphics,    // OpenGL drawing backend.
+    /// OpenGL drawing backend.
+    gl: GlGraphics,
+    /// Graphics Window
     window: Window,
+    /// Graphics Events
     events: Events,
 }
 
 impl Screen {
+    /// Create a new screen 
     fn new(version: &str, width: u32, height: u32) -> Screen {
         // Change this to OpenGL::V2_1 if not working.
         let opengl = match version {
@@ -64,6 +81,7 @@ impl Screen {
         }
     }
 
+    /// Run the application indefinitely until player presses esc.
     fn run(&mut self) {
         while let Some(e) = self.events.next(&mut self.window) {
             if let Some(r) = e.render_args() {
@@ -76,12 +94,14 @@ impl Screen {
         }
     }
 
+    /// Draws the maze on the graphics window.
     fn render(&mut self, args: &RenderArgs) {
 
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-        const GRAY: [f32; 4] = [0.5, 0.5, 0.5, 1.0];
+        /// Arrays containing colors to be used
+        const BLACK: [f32; 4]  = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4]  = [1.0, 1.0, 1.0, 1.0];
+        const _RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const _GRAY: [f32; 4]  = [0.5, 0.5, 0.5, 1.0];
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen
@@ -89,34 +109,19 @@ impl Screen {
 
             let (maze, dim) = generate_maze(); 
 
-            // buffer is the space inbetween the max dimesion of the window and
-            // where the maze starts and stops at
-            // space inbetween top of the window and top of the maze
-            // space inbetween the left side of the window and the left size of the maze
-            // "" right, bottom
+            // buffer_h - buffer height, is the distance between the top of the screen and 
+            // where the maze starts vertically.
             let buffer_h: f64 = (args.height as f64) / 10.0 / 2.0;
+
+            // buffer_w - buffer width, is the distance between the left side of the screen and 
+            // where the maze starts horizontally.
             let buffer_w: f64 = (args.width as f64)  / 10.0 / 2.0;
 
-            // space is the amount of space each element in the vector represents in the maze
+            // space_h - space height, is the amount of space each element in the maze vector take up vertically.
             let space_h: f64  = ((args.height as f64) - buffer_h * 2.0) / (dim[0] as f64);
+            // space_w - space width, is the amount of space each element in the maze vector take up horizontally.
             let space_w: f64  = ((args.width as f64) -  buffer_w * 2.0) / (dim[1] as f64);
-
-            println!("start width = {}", buffer_w);
-            println!("total width = {}", ((args.width as f64) - buffer_w * 2.0));
-            println!("space_w = {}", space_w);
             
-            println!("transform = {:?}", c.transform);
-            Rectangle::new(RED)
-                .draw([buffer_w, buffer_h,
-                       ((args.width as f64) - buffer_w * 2.0),
-                       ((args.height as f64) - buffer_h * 2.0)
-                      ],
-                      &c.draw_state,
-                      c.transform,
-                      gl
-                     );
-
-
             for j in 0..dim[1] {
                 for i in 0..dim[0] {
 
@@ -136,6 +141,7 @@ impl Screen {
         });
     }
 
+    /// Currently not used
     fn update(&mut self, _args: &UpdateArgs) {
         // Rotate 2 radians per second.
 
@@ -143,6 +149,7 @@ impl Screen {
     }
 }
 
+/// Start up maze.
 fn main() {
     let mut screen = Screen::new("V3_2", 500, 500);
     screen.run();
